28616973c35eb0c72aecac61ba45c7de8d2f71d0
boosun13 <double0two22@gmail.com>
2020-07-25 16:45:35 +0900

    device導入 modalwindow導入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
-    module ApplicationHelper
-        def bs4_bgcolor_for(flash_key)
-            case flash_key
+    def bs4_bgcolor_for(flash_key)
+        case flash_key
             when "success"
                 "alert-success"
             when "error"
@@ -10,9 +9,25 @@ module ApplicationHelper
                 "alert-warning"
             when "notice"
                 "alert-info"
-            else
-                flash_key.to_s
-            end
+        else
+            flash_key.to_s
         end
+    end
+
+
+    def resource_name
+        :user
+    end
+
+    def resource
+        @resource ||= User.new
+    end
+
+    def resource_class
+        User
+    end
+
+    def devise_mapping
+        @devise_mapping ||= Devise.mappings[:user]
     end
 end
