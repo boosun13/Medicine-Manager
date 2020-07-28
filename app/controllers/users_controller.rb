2c38946e8547bdbec3998779963d5fc44e468ecf
boosun13 <double0two22@gmail.com>
2020-07-28 23:07:21 +0900

    もっと表記する機能追加 by gem kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def show
+    @prescriptions = current_user.prescriptions.page(params[:page]).per(2)
   end
 end
--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,3 +1,6 @@
 class Prescription < ApplicationRecord
     belongs_to :user
+
+    default_scope -> { order(created_at: :desc) }
+    paginates_per 4
 end
