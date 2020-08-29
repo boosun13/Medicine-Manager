8385051e0647cfc6b9c32e7f2f7a72ad899c0f19
boosun13 <double0two22@gmail.com>
2020-08-30 00:55:51 +0900

    残薬表記

--- app/controllers/checkers_controller.rb
+++ app/controllers/checkers_controller.rb
@@ -5,11 +5,13 @@ class CheckersController < ApplicationController
         @checker.medicine_id = params[:medicine_id].to_i
         @checker.timing = params[:timing].to_i
         @checker.check_date = params[:check_date]
+        @checker.dose = params[:dose]
 
         if @checker.save
             @week = params[:medicine_id]
             @prescription = Prescription.find(params[:prescription_id])
             @checked = params[:check_date]
+            @line = @checker.timing
         else
             @week = params[:medicine_id]
         end
@@ -23,6 +25,7 @@ class CheckersController < ApplicationController
         @prescription = Prescription.find(params[:prescription_id])
         @checked = @checker.check_date.to_date
         @week = @checker.medicine_id
+        @line = @checker.timing
 
 
         @checker.destroy
