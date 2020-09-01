d1a549f51857e62f41352a85ea64404449264234
boosun13 <double0two22@gmail.com>
2020-09-02 01:54:43 +0900

    データベース変更

--- db/migrate/20200826152743_rename_type_column_to_medicines.rb
+++ db/migrate/20200826152743_rename_type_column_to_medicines.rb
@@ -1,6 +1,5 @@
 class RenameTypeColumnToMedicines < ActiveRecord::Migration[5.2]
   def change
     rename_column :medicines, :type, :medicine_type
-    rename_column :medicines, :Detail_type,  :detail_type
   end
 end
